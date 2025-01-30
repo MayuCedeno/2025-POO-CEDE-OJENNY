76b781a77bf42c082ae0cb54e96d43e80458ed8b
Jenny Cedeño <jm.cedenor@uea.edu.ec>
2025-01-30 08:32:43 -0500

    Tarea semana 8 - Adaptación de Proyecto

---
+++ Dashboard.py
@@ -0,0 +1,42 @@
+import os
+def mostrar_codigo(ruta_script):
+    # Asegúrate de que la ruta al script es absoluta
+    ruta_script_absoluta = os.path.abspath(ruta_script)
+    try:
+        with open(ruta_script_absoluta, 'r') as archivo:
+            print(f"\n--- Código de {ruta_script} ---\n")
+            print(archivo.read())
+    except FileNotFoundError:
+        print("El archivo no se encontró.")
+    except Exception as e:
+        print(f"Ocurrió un error al leer el archivo: {e}")
+def mostrar_menu():
+    # Define la ruta base donde se encuentra el dashboard.py
+    ruta_base = os.path.dirname(__file__)
+    opciones = {
+        '1': 'Semana 2 - BLOQUE 1/1.1.Semana 2 - Desarrollo de Ejemplos de Técnicas de Programación (POO)py',
+        '2': 'Semana 3 - BLOQUE 1/2.1.Semana 3 - Comparación de Programación Tradicional y POO en Python.py',
+        '3': 'Semana 4 - BLOQUE 1/3.1.EjemplosMundoReal_POO.py',
+        '4': 'Semana 5 - BLOQUE 1/4.1.tipos de datos, identificadores y convenciones de nomenclatura en Python.',
+        '5': 'Semana 6 - BLOQUE 1/5.1.Unidad 2 - Clases, objetos, herencia, encapsulamiento y polimorfismo.py',
+        '6': 'Semana 7 - BLOQUE 1/6.1.Unidad 2 - Constructores y Destructores.py',
+        # Agrega aquí el resto de las rutas de los scripts
+    }
+    while True:
+        print("\n********Menu Principal - Dashboard*************")
+        # Imprime las opciones del menú
+        for key in opciones:
+            print(f"{key} - {opciones[key]}")
+        print("0 - Salir")
+        eleccion = input("Elige un script para ver su código o '0' para salir: ")
+        if eleccion == '0':
+            break
+        elif eleccion in opciones:
+            # Asegura que el path sea absoluto
+            ruta_script = os.path.join(ruta_base, opciones[eleccion])
+            mostrar_codigo(ruta_script)
+        else:
+            print("Opción no válida. Por favor, intenta de nuevo.")
+# Ejecutar el dashboard
+if __name__ == "__main__":
+    mostrar_menu()
